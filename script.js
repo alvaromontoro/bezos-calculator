9bb308427d834feb88051bae42d4d24e6f667c37
Alvaro Montoro <alvaromontoro@gmail.com>
2021-04-13 23:20:46 -0500

    Fix bug in Safari
    maximumFractionDigits requires minimumFractionDigits in Safari or it breaks

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const numColors = 8;
 const numTiming = 5;
 const numTops = 3;
 const earningRate = 3605;
-const numberFormatter = new Intl.NumberFormat('en', { style: 'currency', currency: 'USD', maximumFractionDigits: 0 });
+const numberFormatter = new Intl.NumberFormat('en', { style: 'currency', currency: 'USD', maximumFractionDigits: 0, minimumFractionDigits: 0 });
 let amount = 0;
 
 function createBubble(cost, text) {
