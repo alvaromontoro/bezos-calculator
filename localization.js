6ca4e0b6c1c8620d8b280e286493870e2a38107a
Alvaro Montoro <alvaromontoro@gmail.com>
2021-05-31 18:15:20 -0500

    choose localization
    Add option for user to pick the localization:
    - Button with language options
    - Popmenu with options
    
    Add internationalization for Spanish/English

--- localization.js
+++ localization.js
@@ -5,11 +5,21 @@ function changeLanguage(newLang) {
   if (validLanguages.indexOf(newLang) > -1) {
     language = newLang;
     document.querySelector("html").setAttribute("lang", language);
+
     document.querySelector("#top").innerHTML = local[language].top;
     document.querySelector("#bottom").innerHTML = local[language].bottom;
     document.querySelector("#link").innerHTML = local[language].link;
     document.querySelector("#change-light").innerHTML = local[language].changeLight;
     document.querySelector("#change-dark").innerHTML = local[language].changeDark;
+
+    const earningRate = 3605;
+    const bubbles = document.querySelectorAll(".bubble");
+    bubbles.forEach(bubble => {
+      const cost = bubble.id.replace("b", "");
+      bubble.innerHTML = `<b>${prettyPrintTime(Math.floor(parseInt(cost) / earningRate))}</b>:<br>${local[language].messages[cost]}<br/><i>${currencyFormatter.format(parseInt(cost))}</i>`
+    });
+
+    document.querySelector("#language-popup").classList.remove("active");
   }
 }
 
